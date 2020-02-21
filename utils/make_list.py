5f93a844987615124805a05ad0ebc7a3436bc301
lingmk <mingkun.ling@qq.com>
2020-02-21 13:37:59 +0800

    make_list.py添加注释

--- utils/make_list.py
+++ utils/make_list.py
@@ -5,44 +5,50 @@ from sklearn.utils import shuffle
 
 def _get_image_label_dir():
     """
+    全部是自己手写的代码
+    From mine own
+    github:
     遍历服务器image和label目录，将image和label一一对应
-    :return: 生成器（image绝对路径，label绝对路径，image文件名，label文件名）
+    :return: 生成器（image绝对路径，label绝对路径）
     """
     data_err = 'data error. check!'
-    image_base = os.path.join('/root/data/LaneSeg/Image_Data')
-    label_base = os.path.join('/root/data/LaneSeg/Gray_Label')
-    for road in os.listdir(image_base):
-        image_road = os.path.join(image_base, road)
-        label_road = os.path.join(label_base, 'Label_' + str.lower(road))
+    image_base = os.path.join('/root/data/LaneSeg/Image_Data')  # 服务器上Image根目录
+    label_base = os.path.join('/root/data/LaneSeg/Gray_Label')  # 服务器上Label根目录
+
+    for road in os.listdir(image_base):  # 遍历根目录下所有目录
+        image_road = os.path.join(image_base, road)  # image的Road02-Road04
+        label_road = os.path.join(label_base, 'Label_' + str.lower(road))  # label的Label_road02-Label_road04
         if not (os.path.isdir(image_road) and
                 os.path.exists(label_road) and
                 os.path.isdir(label_road)):
-            print(image_road, label_road, data_err)
+            print(image_road, label_road, data_err)  # 路径不存在打印显示，跳过
             continue
-        for record in os.listdir(image_road):
-            image_record = os.path.join(image_road, record)
-            label_record = os.path.join(label_road, 'Label/' + record)  # 服务器上label多了一层Label
+        for record in os.listdir(image_road):  # 遍历road下所有目录
+            image_record = os.path.join(image_road, record)  # image的 Record001-Record007
+            label_record = os.path.join(label_road, 'Label/' + record)  # lable的Record001-Record007，比image多了一层Label
             if not (os.path.isdir(image_record) and
                     os.path.exists(label_record) and
                     os.path.isdir(label_record)):
-                print(image_record, label_record, data_err)
+                print(image_record, label_record, data_err)  # 路径不存在打印显示，跳过
                 continue
-            for camera in os.listdir(image_record):
-                image_camera = os.path.join(image_record, camera)
-                label_camera = os.path.join(label_record, camera)
+            for camera in os.listdir(image_record):  # 遍历record下所有目录
+                image_camera = os.path.join(image_record, camera)  # image的Camera5-Camera6
+                label_camera = os.path.join(label_record, camera)  # label的Camera5-Camera6
                 if not (os.path.isdir(image_camera) and
                         os.path.exists(label_camera) and
                         os.path.isdir(label_camera)):
-                    print(image_camera, label_camera, data_err)
+                    print(image_camera, label_camera, data_err)  # 路径不存在打印显示，跳过
                     continue
-                for image in os.listdir(image_camera):
-                    image_abspath = os.path.join(image_camera, image)
-                    label_abspath = os.path.join(label_camera, image.replace('.jpg', '_bin.png'))
+                for image in os.listdir(image_camera):  # 遍历Camera下所有图片
+                    image_abspath = os.path.join(image_camera, image)  # image
+                    label_abspath = os.path.join(label_camera,
+                                                 image.replace('.jpg', '_bin.png'))  # label名字比image多_bin，格式png
                     if not (os.path.isfile(image_abspath) and
                             os.path.exists(label_abspath) and
                             os.path.isfile(label_abspath)):
-                        print(image_abspath, label_abspath, data_err)
-                    yield image_abspath, label_abspath
+                        print(image_abspath, label_abspath, data_err)  # 图片不存在或不对应打印显示，跳过
+                        continue
+                    yield image_abspath, label_abspath  # 生成器函数返回
     pass
 
 
@@ -52,31 +58,35 @@ def _make_data_list(save_path):
     :param save_path: 保存的路径
     :return:
     """
-    g = _get_image_label_dir()
-    abspaths = list(g)
+    g = _get_image_label_dir()  # 获取生成器
+    abspaths = list(g)  # 将生成器转换为列表
 
     df = pd.DataFrame(
-        data=abspaths,
-        columns=['image', 'lable']
+        data=abspaths,  # csv文件数据，每个元素是一条数据
+        columns=['image', 'lable']  # 两列 image、lable
     )
 
-    df_shuffle = shuffle(df)
+    df_shuffle = shuffle(df)  # 随机打乱顺序
     df_shuffle.to_csv(save_path, index=False)  # 不保存行索引（行号）
 
-    # 70%做训练，20%做推断，10%做测试
+    # 70%做训练，20%做推断，剩余10%做测试
     train_size = int(df_shuffle.shape[0] * 0.7)
     valid_size = int(df_shuffle.shape[0] * 0.2)
 
-    print('total: {:d} | train: {:d} | val: {:d} | test: {:d}'.format(df_shuffle.shape[0], train_size, valid_size,
-                                                                      df_shuffle.shape[0] - train_size - valid_size))
+    print('total: {:d} | train: {:d} | val: {:d} | test: {:d}'.format(
+        df_shuffle.shape[0], train_size, valid_size,
+        df_shuffle.shape[0] - train_size - valid_size))
 
-    df_train = df_shuffle[0: train_size]
-    df_valid = df_shuffle[train_size: train_size + valid_size]
-    df_test = df_shuffle[train_size + valid_size:]
+    df_train = df_shuffle[0: train_size]  # train数据集
+    df_valid = df_shuffle[train_size: train_size + valid_size]  # valid数据集
+    df_test = df_shuffle[train_size + valid_size:]  # test数据集
 
-    df_train.to_csv(os.path.join(os.path.dirname(save_path), 'train.csv'), index=False)
-    df_valid.to_csv(os.path.join(os.path.dirname(save_path), 'valid.csv'), index=False)
-    df_test.to_csv(os.path.join(os.path.dirname(save_path), 'test.csv'), index=False)
+    df_train.to_csv(os.path.join(os.path.dirname(save_path), 'train.csv'),  # 保存trian.csv文件
+                    index=False)
+    df_valid.to_csv(os.path.join(os.path.dirname(save_path), 'valid.csv'),  # 保存valid.csv文件
+                    index=False)
+    df_test.to_csv(os.path.join(os.path.dirname(save_path), 'test.csv'),  # 保存test.csv文件
+                   index=False)
 
 
 if __name__ == '__main__':
@@ -84,9 +94,9 @@ if __name__ == '__main__':
     单元测试
     """
 
-    save_path = os.path.join(os.pardir, 'data_list')
-    save_path = os.path.join(save_path, 'all.csv')
-    _make_data_list(save_path)
+    save_path = os.path.join(os.pardir, 'data_list')  # 保存全部数据的路径
+    save_path = os.path.join(save_path, 'all.csv')  # 保存全部数据
+    _make_data_list(save_path)  # 生成csv文件
 
 
     def test__get_image_label_dir():
