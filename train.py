fd214edfeeea3944c3da24f1dcfb84b9e4774595
lingmk <mingkun.ling@qq.com>
2020-02-29 03:22:39 +0800

    train跑起来，效果不好

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
 import torch.utils.data as data
 from torch.autograd import Variable
@@ -12,23 +13,29 @@ from config import Config
 
 
 def cross_entropy2d(output, target, weight=None, size_average=True):
-    # input: (n, c, h, w), target: (n, c, h, w)但target的c=1
-    n, c, h, w = output.size()
-    target = target.squeeze(1).type(torch.int64)  # target(n, h, w)
-
-    # log_p: (n, c, h, w)
-    log_p = F.log_softmax(output, dim=1)
-    # log_p: (n*h*w, c)
-    log_p = log_p.transpose(1, 2).transpose(2, 3).contiguous()
-    log_p = log_p[target.view(n, h, w, 1).repeat(1, 1, 1, c) >= 0]
-    log_p = log_p.view(-1, c)
-    # target: (n*h*w,)
-    mask = target >= 0
-    target = target[mask]
-    loss = F.nll_loss(log_p, target, weight=weight, reduction='sum')
-    if size_average:
-        loss /= mask.data.sum()
-    return loss
+    channels = output.shape[1]
+    output = output.view(output.shape[0], output.shape[1], -1)
+    output = output.permute((0, 2, 1))
+    output = output.contiguous().view(-1, channels)
+    target = target.type(torch.int64).view(-1)
+    return nn.CrossEntropyLoss()(output, target)
+    # # input: (n, c, h, w), target: (n, c, h, w)但target的c=1
+    # n, c, h, w = output.size()
+    # target = target.squeeze(1).type(torch.int64)  # target(n, h, w)
+    #
+    # # log_p: (n, c, h, w)
+    # log_p = F.softmax(output, dim=1)
+    # # log_p: (n*h*w, c)
+    # log_p = log_p.transpose(1, 2).transpose(2, 3).contiguous()
+    # log_p = log_p[target.view(n, h, w, 1).repeat(1, 1, 1, c) >= 0]
+    # log_p = log_p.view(-1, c)
+    # # target: (n*h*w,)
+    # mask = target >= 0
+    # target = target[mask]
+    # loss = F.cross_entropy(log_p, target, weight=weight, reduction='sum')
+    # if size_average:
+    #     loss /= mask.data.sum()
+    # return loss
 
 
 def epoch_timer(func):
@@ -50,8 +57,52 @@ def epoch_timer(func):
     return timer
 
 
+def confusion_matrix(pred, label, n_class):
+    """
+    计算一张图像的混淆矩阵
+    cm = torch.bincount(n_class * label[mask] + pred[mask], minlength=n_class ** 2)
+    n_class * label[mask] 将0,1,2,...,n映射到0,1,2,..,n*n
+    n_class * label[mask] + pred[mask] 将分类映射到不同的grand true
+    比如
+    n=8
+    label=0,pred=0,1,2,...,7映射后是0,1,2,...,7
+    label=1,pred=0,1,2,...,7映射后是8,9,10,...,15
+    ...
+    label=7,pred=0,1,2,...,7映射后是56,57,58,...,63
+    bincount 统计以后就会对应到0,1,2,...,63，再reshape就得到了混淆矩阵
+    :param pred: 估值
+    :param label: grand true
+    :param n_class: n种分类
+    :return: 混淆矩阵
+    """
+    pred, label = pred.type(torch.int), label.type(torch.int)  # 必须转化为int类型否则bincount报错
+    mask = (label >= 0) & (label < n_class)  # 生成掩码，经过掩码以后tensor变为1维tensor
+    cm = torch.bincount(n_class * label[mask] + pred[mask], minlength=n_class ** 2)  # 统计0-n*n的个数
+    return cm.reshape((n_class, n_class))  # 返回reshape成矩阵的confusion matrix
+
+
+def get_acc(cm, n_class):
+    return
+
+
+def get_miou(cm, n_class):
+    """
+    计算mIoU
+    diag是对角线元素，也就是各个分类的TP
+    np.sum(cm, axis=0)也就是各个分类的TP+FP
+    np.sum(cm, axis=1)也就是各个分类的TP+FN
+    iou是各个分类的IoU=TP/(TP+FP+FN)
+    返回是平均IoU将各分类IoU相加再除以分类数
+    :param cm:
+    :param n_class:
+    :return:
+    """
+    iou = torch.diag(cm) / (torch.sum(cm, axis=0) + torch.sum(cm, axis=1) - torch.diag(cm))
+    return torch.sum(iou) / n_class
+
+
 @epoch_timer  # 记录一个epoch时间并打印
-def epoch_train(net, loss_func, optimizer, train_data, valid_data):
+def epoch_train(net, loss_func, optimizer, train_data, n_class):
     """
     一个epoch训练过程，分成两个阶段：先训练，再验证
     :param net: 使用的模型
@@ -66,7 +117,8 @@ def epoch_train(net, loss_func, optimizer, train_data, valid_data):
     # loss_func.to(device)  # loss函数装入CPU
 
     """训练"""
-    train_loss, train_acc = 0., 0.  # 一个epoch训练的loss和正确率acc
+    train_loss = 0.  # 一个epoch训练的loss
+    train_cm = torch.zeros((n_class, n_class)).to(device)  # 一个epoch的混淆矩阵
     net.train()  # 训练
     for i, (train_image, train_label) in enumerate(train_data):
         train_image = Variable(train_image.to(device))  # 一个训练batch image
@@ -79,13 +131,25 @@ def epoch_train(net, loss_func, optimizer, train_data, valid_data):
         optimizer.step()  # 步进
 
         train_loss += loss.detach().cpu().numpy()  # 累加训练batch的loss
-        train_acc += get_acc(train_output, train_label)  # 累加训练batch的acc
+
+        train_pred = F.softmax(train_output, dim=1)  # softmax
+        train_pred = train_pred.detach()  #
+        train_pred = torch.argmax(train_pred, dim=1)  # 将输出转化为dense prediction，减少一个C维度
+        train_label = train_label.detach().squeeze(1)  # label也减少一个C维度
+        train_cm += confusion_matrix(train_pred, train_label, n_class)  # 计算混淆矩阵并累加
         pass
     train_loss /= len(train_data)  # 求取一个epoch训练的loss
-    train_acc /= len(train_data)  # 求取一个epoch训练的acc
+    train_miou = get_miou(train_cm, n_class)
 
+    return train_loss, train_miou
+
+
+@epoch_timer
+def epoch_valid(net, loss_func, valid_data, n_class):
     """验证"""
-    valid_loss, valid_acc = 0., 0.  # 一个epoch验证的loss和正确率acc
+    device = Config.DEVICE  # 使用GPU，没有GPU就用CPU
+    valid_loss = 0.  # 一个epoch验证的loss和正确率acc
+    valid_cm = torch.zeros((n_class, n_class)).to(device)
     net.eval()  # 验证
     for i, (valid_image, valid_label) in enumerate(valid_data):
         valid_image = Variable(valid_image.to(device))  # 一个验证batch image
@@ -96,11 +160,16 @@ def epoch_train(net, loss_func, optimizer, train_data, valid_data):
         # 验证的时候不进行反向传播
 
         valid_loss += loss.detach().cpu().numpy()  # 累加验证batch的loss
-        valid_acc += get_acc(valid_output, valid_label)  # 累加验证batch的acc
+
+        valid_pred = F.softmax(valid_output, dim=1)  # softmax
+        valid_pred = valid_pred.detach()  # 从cuda读取模型输出到cpu转换为numpy
+        valid_pred = torch.argmax(valid_pred, dim=1)  # 将输出转化为dense prediction
+        valid_label = valid_label.detach().squeeze(1)  # label转换为numpy
+        valid_cm += confusion_matrix(valid_pred, valid_label, n_class)  # 计算混淆矩阵并累加
         pass
     valid_loss /= len(valid_data)  # 求取一个epoch验证的loss
-    valid_acc /= len(valid_data)  # 求取一个epoch验证的acc
-    return train_loss, train_acc, valid_loss, valid_acc
+    valid_miou = get_miou(valid_cm, n_class)
+    return valid_loss, valid_miou
 
 
 def save_checkpoint(net, name, epoch):
@@ -118,7 +187,7 @@ def save_checkpoint(net, name, epoch):
     pass
 
 
-def train(net, loss_func, optimizer, train_data, valid_data, name, epochs=5):
+def train(net, loss_func, optimizer, train_data, valid_data, n_class, name, epochs=5):
     """
     训练
     :param net: 被训练的模型
@@ -131,33 +200,24 @@ def train(net, loss_func, optimizer, train_data, valid_data, name, epochs=5):
     :return:
     """
     for e in range(epochs):
-        t_loss, t_acc, v_loss, v_acc = epoch_train(net, loss_func, optimizer,
-                                                   train_data, valid_data)  # 一个epoch训练
-        epoch_str = ('Epoch: {:d} | '
-                     'Train Loss: {:.4f} | Train Acc: {:.4f} | '
-                     'Valid Loss: {:.4f} | Valid Acc: {:.4f}')
-        print(epoch_str.format(e + 1, t_loss, t_acc, v_loss, v_acc))  # 打印一个epoch的loss和acc
+        print('Epoch: {:d}'.format(e + 1))
+
+        # 一个epoch训练
+        t_loss, t_miou = epoch_train(net, loss_func, optimizer, train_data, n_class)
+        train_str = 'Train Loss: {:.4f} | Train mIoU: {:.4f}'
+        print(train_str.format(t_loss, t_miou))
+
+        # 一个epoch验证
+        v_loss, v_miou = epoch_valid(net, loss_func, valid_data, n_class)
+        valid_str = 'Valid Loss: {:.4f} | Valid mIoU: {:.4f}'
+        print(valid_str.format(v_loss, v_miou))
+
         save_checkpoint(net, name, e)  # 每个epoch的参数都保存
         pass
     pass
 
 
 pass
-
-
-def get_acc(output, label):
-    """
-    计算正确率
-    :param output: 模型的输出
-    :param label: label
-    :return: 正确率
-    """
-    # total = output.shape[0]  # 总数
-    # pred = torch.argmax(output, dim=1)  # 模型channel方向最大值的索引也就是估值
-    # num_correct = (pred == label).sum().cpu().numpy()  # 估值与label一致的总数
-    # return num_correct / total  # 正确率=估值正确总数/总数
-    return 1
-
 
 if __name__ == '__main__':
     """
@@ -184,11 +244,13 @@ if __name__ == '__main__':
                                 batch_size=Config.TRAIN_BATCH_SIZE),
     }
 
-    model = FCN8s(n_class=8)
+    num_class = 8
+    model = FCN8s(n_class=num_class)
     # model = unet(3, 8, upmode='upsample', padding=31)
     print(model)
     custom_loss_func = cross_entropy2d  # loss函数
     custom_optimizer = torch.optim.Adam(model.parameters())  # 将模型参数装入优化器
 
     train(model, custom_loss_func, custom_optimizer,
-          data_loaders['train'], data_loaders['valid'], name='fcn8s')  # 开始训（炼）练（丹）
+          data_loaders['train'], data_loaders['valid'],
+          n_class=8, name='fcn8s')  # 开始训（炼）练（丹）
