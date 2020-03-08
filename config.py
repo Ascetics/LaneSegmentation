ef13e97170ad0b332ec3ebc31129b852ea04e492
lingmk <mingkun.ling@qq.com>
2020-03-08 13:57:26 +0800

    调整Config内容

--- config.py
+++ config.py
@@ -11,38 +11,40 @@ class Config(object):
     # 设备   ####################################################################
     # DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     # DEVICE = torch.device('cpu')
-    DEVICE = torch.device('cuda:6')
+    DEVICE = torch.device('cuda:5')
 
     # 数据集 ####################################################################
     DATASETS_ROOT = '/root/private/torch_datasets'  # Pytorch数据集根目录
 
+    # 训练结果###################################################################
+    WEIGHT_SAVE_PATH = '/root/private/LaneSegmentation/weight'  # weight保存路径
+    LOG_FILE = '/root/private/LaneSegmentation/weight/train.log'  # 训练日志
+    TEST_BATCH_SIZE = 1  # 测试集batch为1，这样可以一张图一张图的看
+
+    # 超参数 ####################################################################
+    TRAIN_BATCH_SIZE = 4  # batch大小
+    LEARN_RATE = 0.003  # 学习率
+    WEIGHT_DECAY = 0.0001
+    EPOCHS = 40  # 训练次数
+
+    # 数据处理 ####################################################################
     IMAGE_BASE = '/root/data/LaneSeg/Image_Data'  # image文件的根目录
     LABEL_BASE = '/root/data/LaneSeg/Gray_Label'  # label文件的根目录
 
     TRAIN_RATE = 0.035  # 数据集划分，训练集占整个数据集的比例
     VALID_RATE = 0.010  # 数据集划分，验证集占整个数据集的比例
 
-    DATALIST_TRAIN = '/root/private/LaneSegmentation/data_list/train.csv'  # 车道线分割训练集csv文件路径
-    DATALIST_VALID = '/root/private/LaneSegmentation/data_list/valid.csv'  # 车道线分割验证集csv文件路径
-    DATALIST_TEST = '/root/private/LaneSegmentation/data_list/test.csv'  # 车道线分割测试集csv文件路径
-
-    # 训练结果###################################################################
-    WEIGHT_SAVE_PATH = '/root/private/LaneSegmentation/weight'  # weight保存路径
-    LOG_FILE = '/root/private/LaneSegmentation/weight/train.log' # 训练日志
-    TEST_BATCH_SIZE = 1  # 测试集batch为1，这样可以一张图一张图的看
-
-    # 超参数 ####################################################################
-    TRAIN_BATCH_SIZE = 2  # batch大小
-    LEARN_RATE = 0.003  # 学习率
-    WEIGHT_DECAY = 0.0001
-    EPOCHS = 40  # 训练次数
-
-    # 数据处理 ####################################################################
     IMAGE_NORM_MEAN = [0.485, 0.456, 0.406]  # ImageNet统计的mean
     IMAGE_NORM_STD = [0.229, 0.224, 0.225]  # ImageNet统计的std
     LABEL_NORM_MEAN = [0.5]  # label的mean
     LABEL_NORM_STD = [0.5]  # label的std
-    RESIZE = 448
+    RESIZE = 224
+
+    DATA_LIST = {
+        'train': '/root/private/LaneSegmentation/data_list/train.csv',  # 车道线分割训练集csv文件路径
+        'valid': '/root/private/LaneSegmentation/data_list/valid.csv',  # 车道线分割验证集csv文件路径
+        'test': '/root/private/LaneSegmentation/data_list/test.csv',  # 车道线分割测试集csv文件路径
+    }
 
     # 训练、验证、测试集的image的transforms，用法Config.IMAGE_TRANSFORMS['train']等
     # 经过转换后的image是NCHW的张量
