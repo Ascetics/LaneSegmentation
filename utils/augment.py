038000cbdf17e17ed1dec12f3bd49a99c909ef0b
lingmk <mingkun.ling@qq.com>
2020-03-09 00:52:32 +0800

    Crop只处理一张图像

--- utils/augment.py
+++ utils/augment.py
@@ -27,56 +27,43 @@ class Crop(object):
                      self.start[1] + size[1] if size[1] is not None else size[1],)
         pass
 
-    def __call__(self, images):
+    def __call__(self, image):
         """
         逐个剪裁图像
-        :param images: iterable，图像HWC
+        :param image: iterable，图像HWC
         :return: 剪裁后的图像HWC
         """
-        res = []
-        for im in images:
-            im = np.array(im)
-            if im.ndim == 3:
-                h, w, _ = im.shape
-            if im.ndim == 2:
-                h, w = im.shape
-            assert 0 <= self.start[0] < h and (self.stop[0] is None or 0 <= self.stop[0] < h)  # 剪裁大小不超过原图像大小
-            assert 0 <= self.start[1] < w and (self.stop[1] is None or 0 <= self.stop[1] < w)
 
-            hslice = slice(self.start[0], self.stop[0])  # H方向剪裁量
-            wslice = slice(self.start[1], self.stop[1])  # W方向剪裁量
-            res.append(im[hslice, wslice])
-        return res
+        image = np.asarray(image)
+        if image.ndim == 3:
+            h, w, _ = image.shape
+        if image.ndim == 2:
+            h, w = image.shape
+        assert 0 <= self.start[0] < h and (self.stop[0] is None or 0 <= self.stop[0] < h)  # 剪裁大小不超过原图像大小
+        assert 0 <= self.start[1] < w and (self.stop[1] is None or 0 <= self.stop[1] < w)
+
+        hslice = slice(self.start[0], self.stop[0])  # H方向剪裁量
+        wslice = slice(self.start[1], self.stop[1])  # W方向剪裁量
+        return image[hslice, wslice]
 
     pass
 
 
 if __name__ == '__main__':
-    image = Image.open('Z:/Python资料/AI/cv_lane_seg_初赛/'
-                       'Road04/ColorImage_road04/ColorImage/Record002/Camera 6/'
-                       '171206_054227243_Camera_6.jpg')
-    label = Image.open('Z:/Python资料/AI/cv_lane_seg_初赛/'
-                       'Gray_Label/Label_road04/Label/Record002/Camera 6/'
-                       '171206_054227243_Camera_6_bin.png')
-
-    # t = Crop(offsets=None, size=None)
-    # t = Crop(offsets=None, size=(None, None))
-    # t = Crop(offsets=None, size=(None, 100))
-    # t = Crop(offsets=None, size=(100, None))
-    # t = Crop(offsets=None, size=(500, 500))
-    # t = Crop(offsets=(None, None), size=None)
-    # t = Crop(offsets=(None, 100), size=None)
-    # t = Crop(offsets=(100, None), size=None)
-    # t = Crop(offsets=(100, 100), size=None)
-
+    im = Image.open('Z:/Python资料/AI/cv_lane_seg_初赛/'
+                    'Road04/ColorImage_road04/ColorImage/Record002/Camera 6/'
+                    '171206_054227243_Camera_6.jpg')
+    lb = Image.open('Z:/Python资料/AI/cv_lane_seg_初赛/'
+                    'Gray_Label/Label_road04/Label/Record002/Camera 6/'
+                    '171206_054227243_Camera_6_bin.png')
     t = Crop(offsets=(690, None), size=(None, None))
-    im_crop, lb_crop = t((image, label))
+    im_crop, lb_crop = t((im, lb))
 
     fig, ax = plt.subplots(2, 2, figsize=(20, 10))
     ax = ax.flatten()
-    ax[0].imshow(image)
+    ax[0].imshow(im)
     ax[1].imshow(im_crop)
-    ax[2].imshow(label, cmap='gray')
+    ax[2].imshow(lb, cmap='gray')
     ax[3].imshow(lb_crop, cmap='gray')
     plt.tight_layout()
     plt.show()
