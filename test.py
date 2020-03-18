63417a403f98b00f01f07aee91b70d83ee9c1e42
lingmk <mingkun.ling@qq.com>
2020-03-18 23:55:16 +0800

    test测试无误，可用

--- test.py
+++ test.py
@@ -5,14 +5,15 @@ import matplotlib.pyplot as plt
 
 from PIL import Image
 from train import get_model
+from nets.deeplabv3p import DeepLabV3P
 from utils.laneseg_dataset import LaneSegDataset
 from utils.process_label import gray_to_rgb
 from utils.tools import now_str, get_metrics, get_confusion_matrix, log
-from utils.augment import PairCrop, PairNormalizeToTensor
+from utils.augment import PairCrop, PairNormalizeToTensor, PairResize
 from config import Config
 
 
-def test(net, test_data, device, n_class=8, compare=False):
+def test(net, test_data, device, resize_to=256, n_class=8, compare=False):
     """
     测试
     :param net: AI网络
@@ -25,23 +26,32 @@ def test(net, test_data, device, n_class=8, compare=False):
     net.to(device)
     net.eval()  # 测试
     confusion_matrix = np.zeros((n_class, n_class))  # 记录整个测试的混淆矩阵
+    test_pics_miou = 0.  # 累加每张图像的mIoU
 
     offset = 690  # 剪裁690x3384
     pair_crop = PairCrop(offsets=(offset, None))  # 剪裁690x3384
+    pair_resize = PairResize(size=resize_to)
     pair_norm_to_tensor = PairNormalizeToTensor(norm=True)  # 归一化并正则化
 
     with torch.no_grad():  # 测试阶段，不需要计算梯度，节省内存
         for i_batch, (im, lb) in enumerate(test_data):
-            if i_batch > 10:
-                break
+            # if i_batch > 10:
+            #     break
             im_t, lb_t = pair_crop(im, lb)  # PIL Image,PIL Image
+            im_t, lb_t = pair_resize(im_t, lb_t)  # PIL Image,PIL Image
             im_t, lb_t = pair_norm_to_tensor(im_t, lb_t)  # [C,H,W]tensor,[H,W]tensor
 
             im_t = im_t.to(device)  # [C,H,W]tensor装入GPU
             im_t = im_t.unsqueeze(0)  # 转换为[N,C,H,W] tensor
             output = net(im_t)  # 经过模型输出[N,C,H,W] tensor
             pred = torch.argmax(F.softmax(output, dim=1), dim=1)  # [N,H,W] tensor
-            pred = pred.squeeze(0)  # [H,W]tensor
+
+            pred = pred.unsqueeze(1)  # [N,C,H,W] tensor, F.interpolate操作图像需要[N,C,H,W] tensor
+            pred = pred.type(torch.float)  # 转为float数，F.interpolate只对float类型操作，int，long等都没有实现
+            pred = F.interpolate(pred, size=(lb.size[1] - offset, lb.size[0]),
+                                 mode='nearest')  # pred用nearest差值
+            pred = pred.type(torch.uint8)  # 再转回int类型
+            pred = pred.squeeze(0).squeeze(0)  # [H,W]tensor
             pred = pred.cpu().numpy()  # [H,W]ndarray
 
             supplement = np.zeros((offset, lb.size[0]), dtype=np.uint8)  # [H,W]ndarray,补充成背景
@@ -60,8 +70,9 @@ def test(net, test_data, device, n_class=8, compare=False):
                 ax[1].imshow(gray_to_rgb(np.asarray(lb)))  # 右上角显示 Grand Truth
                 ax[1].set_title('Grand Truth', fontsize=fontsize)  # 标题
 
-                miou = get_metrics(cm, metrics='mean_iou')  # 计算本张图像的mIoU
-                fig.suptitle('mIoU:{:.4f}'.format(miou), fontsize=fontsize)  # 用mIoU作为大标题
+                pic_miou = get_metrics(cm, metrics='mean_iou')  # 计算本张图像的mIoU
+                fig.suptitle('mIoU:{:.4f}'.format(pic_miou), fontsize=fontsize)  # 用mIoU作为大标题
+                test_pics_miou += pic_miou
 
                 mask = (pred != 0).astype(np.uint8) * 255  # [H,W]ndarray,alpha融合的mask
 
@@ -85,17 +96,24 @@ def test(net, test_data, device, n_class=8, compare=False):
                 pass
             pass
         mean_iou = get_metrics(confusion_matrix)  # 整个测试的mIoU
-        log('{:s}\nTest mIoU: {:.4f}\n'.format(now_str(), mean_iou))
+        test_pics_miou /= len(test_data)
+        log('{:s}\nTest mIoU: {:.4f}|Average mIoU of test data:  {:.4f}\n'.format(now_str(), mean_iou, test_pics_miou))
         return mean_iou
 
 
 if __name__ == '__main__':
-    dev = torch.device('cuda:7')  # 选择一个可用的GPU
+    dev = torch.device('cuda:4')  # 选择一个可用的GPU
     load_file = ('/root/private/LaneSegmentation/weight/'
-                 'deeplabv3p_xception-2020-03-17 06:03:02.609908-epoch-14.pth')  # 读取训练好的参数
-    model = get_model('deeplabv3p_xception', in_channels=3, n_class=8, early_weight=load_file)
+                 'deeplabv3p_xception-2020-03-18-15-32-34-epoch-03.pth')  # 读取训练好的参数
+    model = get_model('deeplabv3p_xception',
+                      in_channels=3, n_class=8, device=dev, early_weight=load_file)
+    # model = DeepLabV3P('xception', 3, 8)
+    # wt = torch.load(load_file, map_location=dev)
+    # model.load_state_dict(wt)
+    s = input('->')
     test(net=model,
          test_data=LaneSegDataset(Config.DATA_LIST['valid']),  # 不剪裁，不缩放的测试集，读取PIL Image
+         resize_to=384,  # 这里指定缩放大小
          n_class=8,
          device=dev,
          compare=True)
